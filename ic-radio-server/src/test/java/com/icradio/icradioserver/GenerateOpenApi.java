c1dcb5f665a7f7cd327892f97fbe2d98c021f371
jacharles <jacob.andrew.charles@gmail.com>
2019-04-17 22:00:58 -0400

    added TODO

--- ic-radio-server/src/test/java/com/icradio/icradioserver/GenerateOpenApi.java
+++ ic-radio-server/src/test/java/com/icradio/icradioserver/GenerateOpenApi.java
@@ -1,4 +1,8 @@
 package com.icradio.icradioserver;
 
+/**
+ * TODO: make a mock call to /v2/api-docs?group=angular-client and save the file in the correct location
+ */
 public class GenerateOpenApi {
+
 }
